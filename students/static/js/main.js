6a81467fa7f03909ab1e90e02d8dbe20d8382e34
anna <annapasichnyk77@gmail.com>
2017-02-19 21:35:30 +0200

    refactoring pagination

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    $("tbody tr").hide();
-    size_li = $("tbody tr").size();
+    $("#load tr").hide();
+    size_li = $("#load tr").size();
     x=1;
-    $('tbody tr:lt('+x+')').show();
+    $('#load tr:lt('+x+')').show();
     $('#loadMore').click(function () {
         x= (x+1 <= size_li) ? x+1 : size_li;
-        $('tbody tr:lt('+x+')').show();
+        $('#load tr:lt('+x+')').show();
     });
     $('#showLess').click(function () {
         x=(x-1<0) ? 1 : x-1;
-        $('tbody tr').not(':lt('+x+')').hide();
+        $('#load tr').not(':lt('+x+')').hide();
     });
 });
